8a4f87521146c8b4323437e1fbd12abd9b81db8a
GabeLi69 <gkli@mit.edu>
2021-09-03 15:37:01 +0800

    added submissionService and tagService

---
+++ backend/services/submissionService.js
@@ -0,0 +1,284 @@
+class SubmissionService {
+    constructor(knex){
+        this.knex = knex;
+    }
+
+    //Method to add submission
+    async add(body){
+        if (body.type === "flashcard"){
+            console.log("Adding submission to flashcard");
+            let user_id = await this.knex("user").where({
+                email: body.email
+            }).select("id");
+            return this.knex
+            .insert({
+                user_id: user_id[0].id ,
+                flashcard_id: body.flashcardId,
+                flashcardSubmissionRecording: body.flashcardSubmissionRecording,
+                flashcardSubmissionStatus: true
+            })
+            .into("flashcardSubmission");
+        } 
+        else if (body.type === "dictation"){
+            console.log("Adding submission to dictation");
+            let user_id = await this.knex("user").where({
+                email: body.email
+            }).select("id");
+            return this.knex
+            .insert({
+                user_id: user_id[0].id,
+                dictation_id: body.dictationId,
+                dictationSubmissionPath: body.dictationSubmissionPath,
+                dictationSubmissionStatus: true
+            })
+            .into("dictationSubmission");
+        }
+        else if (body.type === "multipleChoice"){
+            console.log("Adding submission to multipleChoice");
+            let user_id = await this.knex("user").where({
+                email: body.email
+            }).select("id");
+            return this.knex
+            .insert({
+                user_id: user_id[0].id,
+                multipleChoice_id: body.multipleChoiceId,
+                multipleChoiceSubmission: body.multipleChoiceSubmission,
+                multipleChoiceMarking: body.multipleChoiceMarking,
+                multipleChoiceStatus: true
+            })
+            .into("multipleChoiceSubmission");
+        }
+        else if (body.type === "trueFalse") {
+            console.log ("Adding submission to trueFalse")
+            let user_id = await this.knex("user").where({
+                email: body.email
+            }).select("id");
+            return this.knex
+            .insert({
+                user_id: user_id[0].id,
+                trueFalse_id: body.trueFalseId,
+                trueFalseSubmission: body.trueFalseSubmission,
+                trueFalseMarking: body.trueFalseMarking,
+                trueFalseSubmissionStatus: true
+            })
+            .into("trueFalseSubmission");
+        }
+        else {
+            return "card type not recognized";
+        }
+    }
+
+    //Method to delete submission
+    async delete(body){
+        if (body.type === "flashcard"){
+            console.log("Deleting submission from flashcard");
+            return this.knex("flashcardSubmission")
+            .where("flashcardSubmission.id", body.flashcardSubmissionId)
+            .update({
+                flashcardSubmissionStatus: false
+            });
+        
+        } 
+        else if (body.type === "dictation"){
+            console.log("Deleting submission from dictation");
+            return this.knex("dictationSubmission")
+            .where("dictationSubmission.id", body.dictationSubmissionId)
+            .update({
+                dictationcardSubmissionStatus: false
+            });
+        }
+        else if (body.type === "multipleChoice"){
+            console.log("Deleting submission from multipleChoice");
+            return this.knex("multipleChoiceSubmission")
+            .where("multipleChoiceSubmission.id", body.multipleChoiceSubmissionId)
+            .update({
+                multipleChoiceSubmissionStatus: false
+            });
+        }
+        else if (body.type === "trueFalse") {
+            console.log("Deleting submission from trueFalse");
+            return this.knex("trueFalseSubmission")
+            .where("trueFalseSubmission.id", body.trueFalseSubmissionId)
+            .update({
+                trueFalseSubmissionStatus: false
+            });
+        }
+        else {
+            return "card type not recognized";
+        }
+
+    }
+
+    //Method to list details of a submission
+    async submission(body){
+        if (body.type === "flashcard"){
+            console.log("Listing details of flashcardSubmission");
+            return this.knex("flashcardSubmission")
+            .join("user", "flashcardSubmission.user_id", "=", "user.id")
+            .where("flashcardSubmission.id", body.flashcardSubmissionId)
+            .select("user.displayName", "flashcardSubmission.flashcard_id", "flashcardSubmission.id", "flashcardSubmission.flashcardSubmissionRecording")
+            .then((submissions) => {
+                return submissions.map((submission) => {
+                    return ({
+                        displayName: submission.displayName,
+                        flashcardId: submission.flashcard_id,
+                        flashcardSubmissionId: submission.id,
+                        flashcardSubmissionRecording: submission.flashcardSubmissionRecording
+                    });
+                })
+            })
+        
+        } 
+        else if (body.type === "dictation"){
+            console.log("Listing details of dictationSubmission");
+            return this.knex("dictationSubmission")
+            .join("user", "dictationSubmission.user_id", "=", "user.id")
+            .where("dictationSubmission.id", body.dictationSubmissionId)
+            .select("user.displayName", "dictationSubmission.dictation_id", "dictationSubmission.id", "dictationSubmission.dictationSubmissionPath")
+            .then((submissions) => {
+                return submissions.map((submission) => {
+                    return ({
+                        displayName: submission.displayName,
+                        dictationId: submission.dictation_id,
+                        dictationSubmissionId: submission.id,
+                        dictationSubmissionPath: submission.dictationSubmissionPath
+                    });
+                })
+            })
+        
+        } 
+        
+        else if (body.type === "multipleChoice"){
+            console.log("Listing details of multipleChoiceSubmission");
+            return this.knex("multipleChoiceSubmission")
+            .join("user", "multipleChoiceSubmission.user_id", "=", "user.id")
+            .where("multipleChoiceSubmission.id", body.multipleChoiceSubmissionId)
+            .select("user.displayName", "multipleChoiceSubmission.multipleChoice_id", "multipleChoiceSubmission.id", "multipleChoiceSubmission.multipleChoiceSubmission", "multipleChoiceSubmission.multipleChoiceMarking")
+            .then((submissions) => {
+                return submissions.map((submission) => {
+                    return ({
+                        displayName: submission.displayName,
+                        multipleChoiceId: submission.multipleChoice_id,
+                        multipleChoiceSubmissionId: submission.id,
+                        multipleChoiceSubmission: submission.multipleChoiceSubmission,
+                        multipleChoiceSubmissionMarking: submission.multipleChoiceMarking
+                    });
+                })
+            })
+        }
+        else if (body.type === "trueFalse") {
+            console.log("Listing details of trueFalseSubmission");
+            return this.knex("trueFalseSubmission")
+            .join("user", "trueFalseSubmission.user_id", "=", "user.id")
+            .where("trueFalseSubmission.id", body.trueFalseSubmissionId)
+            .select("user.displayName", "trueFalseSubmission.trueFalse_id", "trueFalseSubmission.id", "trueFalseSubmission.trueFalseSubmission", "trueFalseSubmission.trueFalseMarking")
+            .then((submissions) => {
+                return submissions.map((submission) => {
+                    return ({
+                        displayName: submission.displayName,
+                        trueFalseId: submission.trueFalse_id,
+                        trueFalseSubmissionId: submission.id,
+                        trueFalseSubmission: submission.trueFalseSubmission,
+                        trueFalseSubmissionMarking: submission.trueFalseMarking
+                    });
+                })
+            })
+        }
+        else {
+            return "card type not recognized";
+        }
+
+    
+    }
+
+    //Method to list all submissions of a card
+    async list(body){
+        if (body.type === "flashcard"){
+            console.log("Listing all submissions of flashcard");
+            return this.knex("flashcardSubmission")
+            .join("user", "flashcardSubmission.user_id", "=", "user.id")
+            .join("flashcard", "flashcard.id", "=", "flashcardSubmission.flashcard_id")
+            .where("flashcard.id", body.flashcardId)
+            .select("user.displayName", "flashcardSubmission.flashcard_id", "flashcardSubmission.id", "flashcardSubmission.flashcardSubmissionRecording")
+            .then((submissions) => {
+                return submissions.map((submission) => {
+                    return ({
+                        displayName: submission.displayName,
+                        flashcardId: submission.flashcard_id,
+                        flashcardSubmissionId: submission.id,
+                        flashcardSubmissionRecording: submission.flashcardSubmissionRecording
+                    });
+                })
+            })
+        
+        } 
+        else if (body.type === "dictation"){
+            console.log("Listing all submissions of dictation");
+            return this.knex("dictationSubmission")
+            .join("user", "dictationSubmission.user_id", "=", "user.id")
+            .join("dictation", "dictation.id", "=", "dictationSubmission.dictation_id")
+            .where("dictation.id", body.dictationId)
+            .select("user.displayName", "dictationSubmission.dictation_id", "dictationSubmission.id", "dictationSubmission.dictationSubmissionPath")
+            .then((submissions) => {
+                return submissions.map((submission) => {
+                    return ({
+                        displayName: submission.displayName,
+                        dictationId: submission.dictation_id,
+                        dictationSubmissionId: submission.id,
+                        dictationSubmissionPath: submission.dictationSubmissionPath
+                    });
+                })
+            })
+        
+        } 
+        
+        else if (body.type === "multipleChoice"){
+            console.log("Listing all submissions of multipleChoice");
+            return this.knex("multipleChoiceSubmission")
+            .join("user", "multipleChoiceSubmission.user_id", "=", "user.id")
+            .join("multipleChoice", "multipleChoice.id", "=", "multipleChoiceSubmission.multipleChoice_id")
+            .where("multipleChoice.id", body.multipleChoiceId)
+            .select("user.displayName", "multipleChoiceSubmission.multipleChoice_id", "multipleChoiceSubmission.id", "multipleChoiceSubmission.multipleChoiceSubmission", "multipleChoiceSubmission.multipleChoiceMarking")
+            .then((submissions) => {
+                return submissions.map((submission) => {
+                    return ({
+                        displayName: submission.displayName,
+                        multipleChoiceId: submission.multipleChoice_id,
+                        multipleChoiceSubmissionId: submission.id,
+                        multipleChoiceSubmission: submission.multipleChoiceSubmission,
+                        multipleChoiceSubmissionMarking: submission.multipleChoiceMarking
+                    });
+                })
+            })
+        }
+        else if (body.type === "trueFalse") {
+            console.log("Listing all submissions of trueFalse");
+            return this.knex("trueFalseSubmission")
+            .join("user", "trueFalseSubmission.user_id", "=", "user.id")
+            .join("trueFalse", "trueFalse.id", "=", "trueFalseSubmission.trueFalse_id")
+            .where("trueFalse.id", body.trueFalseId)
+            .select("user.displayName", "trueFalseSubmission.trueFalse_id", "trueFalseSubmission.id", "trueFalseSubmission.trueFalseSubmission", "trueFalseSubmission.trueFalseMarking")
+            .then((submissions) => {
+                return submissions.map((submission) => {
+                    return ({
+                        displayName: submission.displayName,
+                        trueFalseId: submission.trueFalse_id,
+                        trueFalseSubmissionId: submission.id,
+                        trueFalseSubmission: submission.trueFalseSubmission,
+                        trueFalseSubmissionMarking: submission.trueFalseMarking
+                    });
+                })
+            })
+        }
+        else {
+            return "card type not recognized";
+        }
+
+
+    }
+}
+
+
+
+
+module.exports = SubmissionService;
---
+++ backend/services/tagService.js
@@ -0,0 +1,210 @@
+class TagService{
+    constructor(knex){
+        this.knex = knex;
+    }
+    
+    //Add tag
+    async add(body){
+        console.log("Checking if tag exists")
+        const tag = await this.knex("tag").where({
+            body: body.tagId
+        })
+        if (tag.length > 0) {
+            if(body.type == "set"){
+                console.log("Tag exists, adding to set")
+                let tag_id = await this.knex("tag").where({
+                    body: body.tagId
+                }).select("id");
+                // let set_id = await this.knex("set").where({
+                //     id: body.location
+                // }).select("id");
+                const query = await this.knex
+                    .insert({
+                        set_id: body.location,
+                        tag_id: tag_id[0].id
+                    })
+                    .into("tag_set")
+                return query
+            }
+            else if(body.type == "classroom"){
+                console.log("Tag exists, adding to classroom")
+                let tag_id = await this.knex("tag").where({
+                    body: body.tagId
+                }).select("id");           
+                const query = await this.knex
+                    .insert({
+                        classroom_id: body.location,
+                        tag_id: tag_id[0].id
+                    })
+                    .into("tag_classroom")
+                return query
+            }
+            else if(body.type == "flashcard"){
+                console.log("Tag exists, adding to flashcard")
+                let tag_id = await this.knex("tag").where({
+                    body: body.tagId
+                }).select("id");
+                const query = await this.knex
+                    .insert({
+                        flashcard_id: body.location,
+                        tag_id: tag_id[0].id
+                    })
+                    .into("tag_flashcard")
+                return query
+            }
+            else if(body.type == "quizcard"){
+                console.log("Tag exists, adding to quizcard")
+                let tag_id = await this.knex("tag").where({
+                    body: body.tagId
+                }).select("id");
+                const query = await this.knex
+                    .insert({
+                        quizcard_id: body.location,
+                        tag_id: tag_id[0].id
+                    })
+                    .into("tag_quizcard")
+                return query
+            }
+        }
+        else {
+            console.log("Tag does not exist, adding to set")
+            return this.knex
+                .insert({
+                    body: body
+                })
+                .into("tag")
+                .then(async () => {
+                    if(body.type == "set"){
+                        console.log("Tag exists, adding to set")
+                        let tag_id = await this.knex("tag").where({
+                            body: body.tagId
+                        }).select("id");
+                        const query = await this.knex
+                            .insert({
+                                set_id: body.location,
+                                tag_id: tag_id[0].id
+                            })
+                            .into("tag_set")
+                        return query
+                    }
+                    else if(body.type == "classroom"){
+                        console.log("Tag exists, adding to classroom")
+                        let tag_id = await this.knex("tag").where({
+                            body: body.tagId
+                        }).select("id");
+                        const query = await this.knex
+                            .insert({
+                                classroom_id: body.location,
+                                tag_id: tag_id[0].id
+                            })
+                            .into("tag_classroom")
+                        return query
+                    }
+                    else if(body.type == "flashcard"){
+                        console.log("Tag exists, adding to flashcard")
+                        let tag_id = await this.knex("tag").where({
+                            body: body.tagId
+                        }).select("id");
+                        const query = await this.knex
+                            .insert({
+                                flashcard_id: body.location,
+                                tag_id: tag_id[0].id
+                            })
+                            .into("tag_flashcard")
+                        return query
+                    }
+                    else if(body.type == "quizcard"){
+                        console.log("Tag exists, adding to quizcard")
+                        let tag_id = await this.knex("tag").where({
+                            body: body.tagId
+                        }).select("id");
+                        const query = await this.knex
+                            .insert({
+                                quizcard_id: body.location,
+                                tag_id: tag_id[0].id
+                            })
+                            .into("tag_quizcard")
+                        return query
+                    }
+            })
+
+        }
+    }
+
+    //Delete tag
+    delete(body){
+        console.log("Removing tag")
+        if(body.type == "set"){
+            return this.knex("tag_set")
+                .where("tag_set.tag_id", body.tagId)
+                .where("tag_set.set_id", body.location)
+                .del()
+        }
+        else if(body.type == "classroom"){
+            return this.knex("tag_classroom")
+                .where("tag_classroom.tag_id", body.tagId)
+                .where("tag_classroom.classroom_id", body.location)
+                .del()
+        }
+        else if(body.type == "flashcard"){
+            return this.knex("tag_flashcard")
+                .where("tag_flashcard.tag_id", body.tagId)
+                .where("tag_flashcard.flashcard_id", body.location)
+                .del()
+        }
+        else if(body.type == "quizcard"){
+            return this.knex("tag_quizcard")
+                .where("tag_quizcard.tag_id", body.tagId)
+                .where("tag_quizcard.quizcard_id", body.location)
+                .del()
+        }
+    }
+
+    //List out all the tags a user has
+    async search(body){
+        console.log("Listing tags")
+        let user_id = await this.knex("user").where({
+            email: body.email
+        }).select("id");
+
+        //This query returns all the tags (in the form of an array) of the user has in his/her classrooms
+        var queryClassroom = this.knex("classroom_user")
+        .join("tag_classroom", "classroom_user.classroom_id", "=", "tag_classroom.classroom_id")
+        .join("tag", "tag_classroom.tag_id", "=", "tag.id")
+        .where("class_user.sharedUser_id", user_id[0].id)
+        .select("tag.id", "tag.body")
+        .then((tags) => {
+            return tags.map((tag) => {
+                return({
+                    tagId: tag.id,
+                    tagBody: tag.body
+                })
+            })
+
+        })
+
+        //This query returns all the tags (in form of an array) of the user has in his/her sets
+        var querySet = this.knex("classroom_user")
+        .join("classroom_set", "classroom_user.classroom_id", "=", "classroom_set.classroom_id")
+        .join("tag_set", "classroom_set.set_id", "=", "tag_set.set_id")
+        .join("tag", "tag_set.tag_id", "=", "tag.id")
+        .select("tag.id", "tag.body")
+        .then((tags) => {
+            return tags.map((tag) => {
+                return({
+                    tagId: tag.id,
+                    tagBody: tag.body
+                })
+            })
+        })
+
+        //concatenate the two arrays above to return all the tags of the user
+        var tags = queryClassroom.concat(querySet);
+        return tags;
+
+        
+    }
+
+}
+
+module.exports = TagService;
