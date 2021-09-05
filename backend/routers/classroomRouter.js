3076a3e79a5a120080c8b9ba5e61967bef889e65
GabeLi69 <gkli@mit.edu>
2021-09-05 18:38:39 +0800

    updated classroomService and setService for bridging, created classroomRouter and sharingRouter

---
+++ backend/routers/classroomRouter.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+class ClassroomRouter {
+    constructor(classroomService){
+        this.classroomService = classroomService;
+    }
+    router(){
+        let router = express.Router();
+
+        router.post("/", this.post.bind(this));
+        router.put("/", this.put.bind(this));
+        router.delete("/", this.delete.bind(this));
+    }
+
+    post(req, res){
+        console.log("Post request to create a classroom ");
+
+        return this.classroomService.add(req.body)
+        .catch((err) => {
+            return res.status(500).json(err);
+        });
+    }
+
+    put(req, res){
+        console.log("Put request to edit a classroom");
+
+        return this.classroomService.edit(req.body)
+        .catch((err) => {
+            return res.status(500).json(err);
+        });
+    }
+    
+    delete(req, res){
+        console.log("Delete request to delete a classroom");
+
+        return this.classroomService.delete(req.body)
+        .catch((err) => {
+            return res.status(500).json(err);
+        })
+
+
+    }
+
+}
+
+module.exports = ClassroomRouter;
