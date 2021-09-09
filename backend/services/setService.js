3527a21df7a8b5e364c9204ea4f9b01a15331f02
GabeLi69 <gkli@mit.edu>
2021-09-09 21:25:07 +0800

    added canvas component, canvas classroom set flashcard actions and reducers

--- backend/services/setService.js
+++ backend/services/setService.js
@@ -1,207 +1,207 @@
 class Set {
-    constructor(knex){
+    constructor(knex) {
         this.knex = knex
     };
 
     //Add a set title, desc according to user
-    async add(body){
+    async add(body) {
         const email = await this.knex("user")
-        .where("email", body.email)
-        .select("id");
+            .where("email", body.email)
+            .select("id");
 
         return this.knex("set")
-        .insert({
-            setTitle: body.title,
-            setDesc: body.desc,
-            user_id: email[0].id,
-            setStatus: true
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+            .insert({
+                setTitle: body.title,
+                setDesc: body.desc,
+                user_id: email[0].id,
+                setStatus: true
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     };
 
     //Edit a specific set title, desc, accord to index
-    edit(body){
+    edit(body) {
         return this.knex("set")
-        .where("id", body.setId)
-        .update({
-            setTitle: body.title,
-            setDesc: body.desc,
-        })
-        .catch((err) => {
-            console.log(err)
-        });
+            .where("id", body.setId)
+            .update({
+                setTitle: body.title,
+                setDesc: body.desc,
+            })
+            .catch((err) => {
+                console.log(err)
+            });
     };
 
     //Make a specific set inactive
-    delete(body){
+    delete(body) {
         return this.knex("set")
-        .where("id", body.setId)
-        .update({
-            setStatus: false,
-        })
-        .catch((err) => {
-            console.log(err)
-        });
+            .where("id", body.setId)
+            .update({
+                setStatus: false,
+            })
+            .catch((err) => {
+                console.log(err)
+            });
     };
 
     //list all details of a specific set
-    set(body){
+    set(body) {
         let setData = {}
         return this.knex("set")
-        .join("user", "set.user_id", "user.id")
-        .where("set.id", body.setId)
-        .where("set.setStatus", true)
-        .select("set.id", "set.setTitle", "set.setDesc", "user.displayName")
-        .then(async (set) => {
-            setData.id = set[0].id,
-            setData.title = set[0].setTitle,
-            setData.description = set[0].setDesc,
-            setData.owner = set[0].displayName
-            var queryFlashcard = await this.knex("set_flashcard").where("set_flashcard.set_id", set[0].id).select("set_flashcard.flashcard_id");
+            .join("user", "set.user_id", "user.id")
+            .where("set.id", body.setId)
+            .where("set.setStatus", true)
+            .select("set.id", "set.setTitle", "set.setDesc", "user.displayName")
+            .then(async (set) => {
+                setData.id = set[0].id,
+                    setData.title = set[0].setTitle,
+                    setData.description = set[0].setDesc,
+                    setData.owner = set[0].displayName
+                var queryFlashcard = await this.knex("set_flashcard").where("set_flashcard.set_id", set[0].id).select("set_flashcard.flashcard_id");
                 setData.bridge_flashcard = queryFlashcard.map((flashcard) => {
-                    return{
-                        flashcard_id:flashcard.flashcard_id
+                    return {
+                        flashcard_id: flashcard.flashcard_id
                     }
                 })
-            var queryQuizcard = await this.knex("set_quizcard").where("set_quizcard.set_id", set[0].id).select("set_quizcard.quizcard_id");
+                var queryQuizcard = await this.knex("set_quizcard").where("set_quizcard.set_id", set[0].id).select("set_quizcard.quizcard_id");
                 setData.bridge_quizcard = queryQuizcard.map((quizcard) => {
-                    return{
-                        quizcard_id:quizcard.quizcard_id
+                    return {
+                        quizcard_id: quizcard.quizcard_id
                     }
                 })
-            var queryDictationcard = await this.knex("set_dictationcard").where("set_dictationcard.set_id", set[0].id).select("set_dictationcard.dictationcard_id");
+                var queryDictationcard = await this.knex("set_dictationcard").where("set_dictationcard.set_id", set[0].id).select("set_dictationcard.dictationcard_id");
                 setData.bridge_dictationcard = queryDictationcard.map((dictationcard) => {
-                    return{
+                    return {
                         dictationcard_id: dictationcard_dictationcard.id
                     }
                 })
-        })
-        .then(()=>{
-            return this.knex("tag_set")
-            .where("set_id", body.setId)
-            .join("tag", "tag_set.tag_id", "tag.id")
-            .select("tag.tagBody", "tag.id")
-            .then((tags) => {
-                setData.tags = tags.map((tag)=>{
-                    return {
-                        id: tag.id,
-                        body:tag.body
-                    };
-                });
             })
-            .then(()=>{
-                return setData
-            })
-        })
-        .catch((err) => {
-            console.log(err)
-        });
-    };
-
-    //list all sets of a classroom
-    list(body){
-        return this.knex("set")
-        .join("classroom_set", "set.id", "classroom_set.set_id")
-        .join("classroom", "classroom_set.classroom_id", "classroom.id")
-        .where("classroom_set.classroom_id", body.classroomId)
-        .andWhere("classroom.classroomStatus", true)
-        .select("set.id", "set.setTitle", "set.setDesc")
-        .then((sets)=>{
-            return sets.map((set) => {
-                return ({
-                    id: set.id,
-                    title: set.setTitle,
-                    description: set.setDesc,
-                });
-            });
-        })
-        .catch((err) => {
-            console.log(err)
-        });
-    };
-
-    //list all sets a user has access to
-    async user(body){
-        const email = await this.knex("user")
-        .where({
-            email: body.email,
-        })
-        .select("id");
-
-        
-
-        return this.knex("set")
-        .where({
-            user_id: email[0].id,
-            setStatus: true
-        })
-        .then((sets)=>{
-            return sets.map((set) => {
-                let setData = {};               
+            .then(() => {
                 return this.knex("tag_set")
-                .where("set_id", set.id)
-                .join("tag", "tag_id", "tag.id")
-                .select("tag.id", "tag.tagBody")
-                .then((tags) => {
-                    return tags.map((tag)=>{
-                        return {
-                            id: tag.id,
-                            body: tag.body
-                        };
-                        });
-                    })
+                    .where("set_id", body.setId)
+                    .join("tag", "tag_set.tag_id", "tag.id")
+                    .select("tag.tagBody", "tag.id")
                     .then((tags) => {
-                        setData.tags = tags
-                    })
-                    .then(() => {
-                        setData.id = set.id
-                        setData.title = set.setTitle
-                        setData.description = set.setDesc
-                       
-                    })
-                    .then(() => {
-                        return this.knex("set_flashcard").where("set_flashcard.set_id", set.id).select("set_flashcard.flashcard_id")
-                    })
-                    .then((flashcards) => {
-                        setData.bridge_flashcard = flashcards.map((flashcard) => {
-                            return{
-                                flashcard_id:flashcard_flashcard.id
-                            }
-                        })
-                        
-                    })
-                    .then(() => {
-                        return this.knex("set_quizcard").where("set_quizcard.set_id", set.id).select("set_quizcard.quizcard_id");
-                    })
-                    .then((quizcards) => {
-                        setData.bridge_quizcard = quizcards.map((quizcard) => {
-                            return{
-                                quizcard_id:quizcard_quizcard.id
-                            }
-                        })
-                    })
-                    .then(() => {
-                        return this.knex("set_dictationcard").where("set_dictationcard.set_id", set.id).select("set_dictationcard.dictationcard_id");
-                    })
-                    .then((dictationcards) => {
-                        setData.bridge_dictationcard = dictationcards.map((dictationcard) =>{
-                            return{
-                                dictationcard_id:dicationcard_dictationcard.id
-                            }
-                        })
+                        setData.tags = tags.map((tag) => {
+                            return {
+                                id: tag.id,
+                                body: tag.body
+                            };
+                        });
                     })
                     .then(() => {
                         return setData
                     })
             })
-        })
+            .catch((err) => {
+                console.log(err)
+            });
+    };
 
-        .catch((err) => {
-            console.log(err)
-        });
+    //list all sets of a classroom
+    list(body) {
+        return this.knex("set")
+            .join("classroom_set", "set.id", "classroom_set.set_id")
+            .join("classroom", "classroom_set.classroom_id", "classroom.id")
+            .where("classroom_set.classroom_id", body.classroomId)
+            .andWhere("classroom.classroomStatus", true)
+            .select("set.id", "set.setTitle", "set.setDesc")
+            .then((sets) => {
+                return sets.map((set) => {
+                    return ({
+                        id: set.id,
+                        title: set.setTitle,
+                        description: set.setDesc,
+                    });
+                });
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+    };
+
+    //list all sets a user has access to
+    async user(body) {
+        const email = await this.knex("user")
+            .where({
+                email: body.email,
+            })
+            .select("id");
+            console.log("EMAIL >>", email)
+        return this.knex("set")
+            .where({
+                user_id: email[0].id,
+                setStatus: true
+            })
+            .then((sets) => {
+                return sets.map((set) => {
+                    let setData = {};
+                    console.log("SET.ID >>", set.id)
+                    return this.knex("tag_set")
+                        .where("set_id", set.id)
+                        .join("tag", "tag_set.tag_id", "tag.id")
+                        .select("tag.id", "tag.tagBody")
+                        .then((tags) => {
+                            return tags.map((tag) => {
+                                return {
+                                    id: tag.id,
+                                    body: tag.tagbody
+                                };
+                            });
+                        })
+                        .then((tags) => {
+                            setData.tags = tags
+                            
+                        })
+                        .then(() => {
+                            setData.id = set.id
+                            setData.title = set.setTitle
+                            setData.description = set.setDesc
+
+                        })
+                        .then(() => {
+                            return this.knex("set_flashcard").where("set_flashcard.set_id", set.id).select("set_flashcard.flashcard_id")
+                        })
+                        .then((flashcards) => {
+                            setData.bridge_flashcard = flashcards.map((flashcard) => {
+                                return {
+                                    flashcard_id: flashcard_flashcard.id
+                                }
+                            })
+
+                        })
+                        .then(() => {
+                            return this.knex("set_quizcard").where("set_quizcard.set_id", set.id).select("set_quizcard.quizcard_id");
+                        })
+                        .then((quizcards) => {
+                            setData.bridge_quizcard = quizcards.map((quizcard) => {
+                                return {
+                                    quizcard_id: quizcard_quizcard.id
+                                }
+                            })
+                        })
+                        .then(() => {
+                            return this.knex("set_dictationcard").where("set_dictationcard.set_id", set.id).select("set_dictationcard.dictationcard_id");
+                        })
+                        .then((dictationcards) => {
+                            setData.bridge_dictationcard = dictationcards.map((dictationcard) => {
+                                return {
+                                    dictationcard_id: dicationcard_dictationcard.id
+                                }
+                            })
+                        })
+                        .then(() => {
+                            return setData
+                        })
+                })
+            })
+
+            .catch((err) => {
+                console.log(err)
+            });
     };
 }
-    
+
 module.exports = Set
---
+++ frontend/src/Component/canvas.js
@@ -0,0 +1,194 @@
+import React from 'react';
+import io from 'socket.io-client';
+import FormData from 'form-data';
+const axios = require("axios");
+
+import { submitCanvas } from "../Redux/fillitin"
+import { connect } from "react-redux";
+
+
+class Canvas extends React.Component{
+
+    timeout;
+
+    ctx;
+    isDrawing = false;
+
+constructor(props){
+    super(props);
+
+    this.socket = io.connect("server");
+
+
+    this.socket.on("clear", () => {
+        var canvas = document.querySelector('#board');
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    });
+
+    this.socket.on("canvas-data", function (data) {
+
+        var root = this;
+        var interval = setInterval(function () {
+            if (root.isDrawing) return;
+            root.isDrawing = true;
+            clearInterval(interval);
+            var image = new Image();
+            var canvas = document.querySelector('#board');
+            var ctx = canvas.getContext('2d');
+            image.onload = function () {
+                ctx.drawImage(image, 0, 0);
+
+                root.isDrawing = false;
+            };
+            image.src = data;
+        }, 10)
+    })
+}
+
+componentDidMount() {
+    this.drawOnCanvas();
+}
+
+componentWillReceiveProps(newProps) {
+    this.ctx.strokeStyle = newProps.color;
+    this.ctx.lineWidth = newProps.size;
+}
+
+drawOnCanvas() {
+    var canvas = document.querySelector('#board');
+    this.ctx = canvas.getContext('2d');
+    var ctx = this.ctx;
+
+    var sketch = document.querySelector('#sketch');
+    var sketch_style = getComputedStyle(sketch);
+    canvas.width = parseInt(sketch_style.getPropertyValue('width'));
+    canvas.height = parseInt(sketch_style.getPropertyValue('height'));
+    var drawing = false;
+    var current = {}
+
+    canvas.addEventListener('mousedown', onMouseDown, false);
+    canvas.addEventListener('mouseup', onMouseUp, false);
+    canvas.addEventListener('mouseout', onMouseUp, false);
+    canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
+
+    //Touch support for mobile devices
+    canvas.addEventListener('touchstart', onMouseDown, false);
+    canvas.addEventListener('touchend', onMouseUp, false);
+    canvas.addEventListener('touchcancel', onMouseUp, false);
+    canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false);
+
+    function onMouseDown(e) {
+        drawing = true;
+        current.x = e.clientX || e.touches[0].clientX;
+        current.y = e.clientY || e.touches[0].clientY;
+    }
+
+    function onMouseUp(e) {
+        if (!drawing) { return; }
+        drawing = false;
+        drawLine(current.x, current.y, e.clientX || e.touches.clientX, e.clientY || e.touches.clientY, current.color, true);
+    }
+
+    function onMouseMove(e) {
+        if (!drawing) { return; }
+        drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true);
+        current.x = e.clientX || e.touches[0].clientX;
+        current.y = e.clientY || e.touches[0].clientY;
+    }
+    function throttle(callback, delay) {
+        var previousCall = new Date().getTime();
+        return function () {
+            var time = new Date().getTime();
+
+            if ((time - previousCall) >= delay) {
+                previousCall = time;
+                callback.apply(null, arguments);
+            }
+        };
+    }
+
+    var root = this;
+    //onPaint
+    var drawLine = function (x0, y0, x1, y1) {
+        ctx.beginPath();
+        ctx.moveTo(x0, y0);
+        ctx.lineTo(x1, y1);
+        ctx.stroke();
+        ctx.closePath();
+
+        var base64ImageData = canvas.toDataURL("image/png");
+        root.socket.emit("canvas-data", base64ImageData);
+
+    };
+
+
+}
+clearcanvas() {
+    var canvas = document.querySelector('#board');
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var base64ImageData = canvas.toDataURL("image/png");
+    this.socket.emit("clear", base64ImageData);
+}
+
+submit(){
+    console.log("HELLO")
+    var canvas = document.querySelector('#board');
+    var ctx = canvas.getContext('2d');
+    var base64ImageData = canvas.toDataURL("image/png");
+    //console.log(base64ImageData)
+
+    const dataURLtoFile = (dataurl, filename) => {
+        const arr = dataurl.split(',')
+        const mime = arr[0].match(/:(.*?);/)[1]
+        const bstr = Buffer.from(arr[1], 'base64').toString('utf-8')
+        //console.log(bstr)
+        let n = bstr.length
+        const u8arr = new Uint8Array(n)
+        while (n) {
+          u8arr[n - 1] = bstr.charCodeAt(n - 1)
+          n -= 1 // to make eslint happy
+        }
+        return new File([u8arr], filename, { type: mime })
+      }
+      
+      // generate file from base64 string
+      const file = dataURLtoFile(`data:image/png;base64,${base64ImageData}`)
+      // put file into form data
+      const data = new FormData()
+      data.append('img', file, file.name)
+
+      //call the action to dispatch the action and post the canvas data to database
+      this.props.submitMDP(data)
+      
+      // now upload (this is gonna be in the action creator)
+}
+
+
+render() {
+    return (
+        <div>
+            <div class="sketch" id="sketch">
+
+                <canvas className="board" id="board"></canvas>
+
+            </div>
+            <button onClick={() => this.clearcanvas()}> Clear </button>
+            <button onClick={() => this.submit()}> Submit </button>
+        </div>
+    )
+}
+
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        submitMDP: (data) => dispatch(submitCanvas(data))
+    }
+}
+
+
+
+export default connect(null, mapDispatchToProps)(Canvas)
