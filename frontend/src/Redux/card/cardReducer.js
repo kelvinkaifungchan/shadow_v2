5902791303297e67005f931a785049dec1a1ef2a
GabeLi69 <gkli@mit.edu>
2021-09-10 16:22:27 +0800

    updated

--- frontend/src/Redux/card/cardReducer.js
+++ frontend/src/Redux/card/cardReducer.js
@@ -3,18 +3,19 @@ import { EDIT_CARD } from "./cardAction";
 import { DELETE_CARD } from "./cardAction";
 
 const initialState = {
-    card: []
+    card: {flashcard:[], quizcard:[], dictationcard:[]}
 };
 
 export function cardReducer(state = initialState, action){
     switch(action.type){
         case ADD_CARD:
+            if(action.payload.type === "flashcard")
             return {
-                card: [...state.card, action.payload]
+                card: {...state.card, flashcard:[...state.card.flashcard, action.payload]}
             };
         case EDIT_CARD:
             return {
-                
+
             }
         case DELETE_CARD:
             return {
--- frontend/src/Redux/classroom/classroomReducer.js
+++ frontend/src/Redux/classroom/classroomReducer.js
@@ -14,7 +14,7 @@ export function classroomReducer(state = initialState, action){
             };
         case EDIT_CLASSROOM:
             var newClassroom = action.payload;
-            var newArray = state.classroom.filter((classroom) => classroom.id !== newClassroom.id);
+            var newArray = state.classroom.filter((classroom) => classroom.classroomId !== newClassroom.id);
             newArray.push(newClassroom);
             return{
                 classroom: newArray
