e1132107f64e13ff25efca2736cfd9f45a853e09
T L <tl@Ts-MacBook-Air.local>
2021-09-13 01:53:36 +0800

    making create flash card

--- frontend/src/Component/headinginput.js
+++ frontend/src/Component/headinginput.js
@@ -5,33 +5,35 @@ import { getdataThunk } from '../Redux/actions/action'
 import classes from './headinginput.module.css'
 
 class PureHeadingInput extends React.Component {
-    render(){
-        return (
-            <>
-      <form className={classes.headingframe}>
-        <input
-          type="text" 
-          placeholder="Untitled Flashcard"
-          className={classes.title}
-        />
-        <input
-          type="text" 
-          placeholder="Insert a description"
-          className={classes.description}
-        />
-      </form>
-            </>
-        )
-    }
+  render() {
+    console.log("this is this.state in heading ip", this.state);
+    return (
+      <>
+        <form className={classes.headingframe}>
+          <input onChange={(e)=>this.props.handleHeading(e.currentTarget.value)}
+            type="text"
+            placeholder="Untitled Flashcard"
+            className={classes.title}
+          />
+          {/* {this.props.card.type ? null : <input
+            type="text"
+            placeholder="Insert a description"
+            className={classes.description}
+          />} */}
+
+        </form>
+      </>
+    )
+  }
 }
 
 
-const mapDispatchToProps  = dispatch => {
-    return {
-        getdata: (email) => {
-            dispatch(getdataThunk(email))
-        }
+const mapDispatchToProps = dispatch => {
+  return {
+    getdata: (email) => {
+      dispatch(getdataThunk(email))
     }
+  }
 }
 
-export const HeadingInput = connect (null, mapDispatchToProps)(PureHeadingInput)
+export const HeadingInput = connect(null, mapDispatchToProps)(PureHeadingInput)
