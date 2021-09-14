c8bf1474128f1df8932355b4035ced11ee40269d
GabeLi69 <gkli@mit.edu>
2021-09-14 14:53:30 +0800

    added bridges to cardAction

--- frontend/src/Redux/actions/cardAction.js
+++ frontend/src/Redux/actions/cardAction.js
@@ -12,32 +12,94 @@ export const DELETE_DICTATIONCARD = "DELETE_DICTATIONCARD";
 export const DELETE_FLASHCARD = "DELETE_FLASHCARD";
 export const DELETE_QUIZCARD = "DELETE_QUIZCARD";
 
+export const ADD_BRIDGE_SET_DICTATIONCARD = "ADD_BRIDGE_SET_DICTATIONCARD";
+export const ADD_BRIDGE_SET_FLASHCARD = "ADD_BRIDGE_SET_FLASHCARD";
+export const ADD_BRIDGE_SET_QUIZCARD = "ADD_BRIDGE_SET_QUIZCARD";
+
 export const addCard = (card) => async (dispatch) => {
     console.log("adding card")
-   const { data } = axios.post("http://localhost:8080/api/card", card);
-   const newId = data[0];
- 
-       console.log("card ID", newId)
+    let newId;
+    if(card.type === "dictationcard"){
+        await axios.post("http://localhost:8080/api/card", card)
+    .then((data) => {
+        newId = data.data[0];
+        console.log("card ID", newId)
+        return newId
+    }).then((newId) =>{
+        return axios.post("http://localhost:8080/api/bridge", {
+            type: card.type,
+            setId: card.setId,
+            dictationcardId: newId
+        })
 
-       if (card.type === "dictationcard") {
+    }).then(() => {
         dispatch({
             type: ADD_DICTATIONCARD,
             payload: {dictationcard_id: newId, user_id: card.userEmail, dictationcardTitle: card.dictationcardTitle, dictationcardRecording: card.dictationcardRecording}
         })
-       }
-       else if (card.type === "flashcard") {
+    }).then(() => {
+        dispatch({
+            type: ADD_BRIDGE_SET_DICTATIONCARD,
+            payload: {id:{set_id: card.setId}, content:{dictationcard_id: newId}}
+        })
+    })
+    }
+
+    if(card.type === "flashcard"){
+        await axios.post("http://localhost:8080/api/card", card)
+    .then((data) => {
+        newId = data.data[0];
+        console.log("card ID", newId)
+        return newId
+    }).then((newId) =>{
+        return axios.post("http://localhost:8080/api/bridge", {
+            type: card.type,
+            setId: card.setId,
+            flashcardId: newId
+        })
+
+    }).then(() => {
         dispatch({
             type: ADD_FLASHCARD,
-            payload: {flashcard_id: newId, user_id: card.userEmail, flashcardTitle: card.flashcardTitle, flashcardBody: card.flashcardBody, flashcardRecording: card.flashcardRecording}
+            payload: {flashcard_id: newId, user_id: card.userEmail, flashcardTitle: card.flashcardTitle, flashcardRecording: card.flashcardRecording}
         })
-       }
-       else if (card.type === "quizcard") {
+    }).then(() => {
+        dispatch({
+            type: ADD_BRIDGE_SET_FLASHCARD,
+            payload: {id:{set_id: card.setId}, content:{flashcard_id: newId}}
+        })
+    })
+    }
+
+    if(card.type === "quizcard"){
+        await axios.post("http://localhost:8080/api/card", card)
+    .then((data) => {
+        newId = data.data[0];
+        console.log("card ID", newId)
+        return newId
+    }).then((newId) =>{
+        return axios.post("http://localhost:8080/api/bridge", {
+            type: card.type,
+            setId: card.setId,
+            quizcardId: newId
+        })
+
+    }).then(() => {
         dispatch({
             type: ADD_QUIZCARD,
             payload: {quizcard_id: newId, user_id: card.userEmail, quizcardTitle: card.quizcardTitle, quizcardRecording: card.quizcardRecording}
         })
-       }
+    }).then(() => {
+        dispatch({
+            type: ADD_BRIDGE_SET_QUIZCARD,
+            payload: {id:{set_id: card.setId}, content:{quizcard_id: newId}}
+        })
+    })
     }
+   
+
+}
+ 
 
 export const editCard = (card) => async (dispatch) => {
     console.log("editing card")
