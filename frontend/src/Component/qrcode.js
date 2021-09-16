df55940f0c688c2d8131490a2a31ec6e38d3cf2d
T L <tl@Marcellade-Air.localdomain>
2021-09-16 22:34:34 +0800

    qr-code

--- frontend/src/Component/qrcode.js
+++ frontend/src/Component/qrcode.js
@@ -1,32 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom';
-import { Modal, ModalHeader, ModalBody, Button} from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, Button } from 'reactstrap';
 
 
-class PureQRModal extends React.Component{
+class PureQRModal extends React.Component {
 
     render() {
-        console.log("PROPS IN SCM",this.props);
+        console.log("PROPS IN SCM", this.props);
         return (
             <div>
                 <Modal isOpen={this.props.modal.modal} toggle={this.props.toggle} >
-                    <ModalHeader toggle={this.toggle}> Select a card type </ModalHeader>
+                    <ModalHeader toggle={this.toggle}>Scan this to open the live canvas</ModalHeader>
                     <ModalBody>
-                        <div className="row">
-                            <div className="col m-3 p-3 border border-4 rounded-lg d-inline-flex">
-                                <Button onClick={(e)=>{this.props.navigate(e)}}>
-                                    <div>Flashcard</div>
-                                <div>Shadow to improve speaking</div>
-                                </Button>
-                            </div>
-                            <div className="col col-4 d-flex justify-content-center">
-                                 <Link className="btn p-2 border" to="/createQuizcard"><h2>Quizcard</h2> <br/> <span>Test your knowledge</span></Link>
-                            </div>
-                            <div className="col col-4 d-flex justify-content-center">
-                                 <Link className="btn p-2 border" to="/createDictationcard"><h2>Dictation Card</h2> <br/> <span>Write</span></Link>
-                            </div>
+                        <div className="qr-code text-center">
+                            <img src="https://chart.googleapis.com/chart?cht=qr&chl=Hello+World&chs=160x160&chld=L|0" />
                         </div>
+
+                        <button >Done</button>
                     </ModalBody>
                 </Modal>
             </div>
@@ -35,4 +26,4 @@ class PureQRModal extends React.Component{
 }
 
 
-export const QRModal = connect(null, null)( PureQRModal)
+export const QRModal = connect(null, null)(PureQRModal)
