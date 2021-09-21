24c2b39e2d1b96f864b416303d1d87fc34a1f0c6
ih810 <ivanhong810@gmail.com>
2021-09-22 02:49:04 +0800

    fixed transcript, upload videos service, new tag modal rerender  create quizcards key, flashcard submissions switching video problem, addnew flashcard submission bug

--- frontend/src/Pages/ViewFlashCard.js
+++ frontend/src/Pages/ViewFlashCard.js
@@ -61,7 +61,7 @@ class ViewFlashCard extends React.Component {
                 transcript: this.state.correctFlashcard.length > 0 && this.state.correctFlashcard[0].flashcardBody
             })
             const correctProps = nextProps.cards.flashcard.filter(filter => filter.id === parseInt(this.props.match.params.id))
-            console.log( "<><><><><><><>HERE U ARE",correctProps);
+            console.log( "<><><><><><><>CorretProps in VFC",correctProps);
             if (this.props.user.role === "teacher") {
                 this.setState({
                     correctSubmission: correctProps[0].submission,
@@ -120,6 +120,7 @@ class ViewFlashCard extends React.Component {
     onClickShowSubmissionViewer(id) {
         const cooresFeed2 = this.props.cards.flashcard.filter((fc) => { return fc.id === parseInt(this.props.match.params.id) })
         const cooresFeed3 = cooresFeed2[0].submission.filter((sub) => { return sub.id === id })
+        console.log('cooresFeed2',cooresFeed2)
         this.setState({
             showRecorder: false,
             showSubmissionViewer: true,
@@ -166,9 +167,10 @@ class ViewFlashCard extends React.Component {
     }
     
     render() {
+        console.log('thisstate',this.state)
+        console.log('this props',this.props)
         return (
             <div className="page">
-                {/* <NavBar user={this.props.user} history={this.props.history}/> */}
 
                 <div className={classes.viewflashcard}>
                     {/* 1st row: Header */}
@@ -179,7 +181,7 @@ class ViewFlashCard extends React.Component {
                     {/* 2nd row: Transcript & Video Player */}
                     <div className="row d-flex p-4">
                         <div className="col-6">
-                            <Transcript title={this.state} transcript={this.state} />
+                            {this.state.correctFlashcard.length > 0 ?<Transcript title={this.state} flashcardBody={this.state.correctFlashcard[0].flashcardTitle} /> : null}
                         </div>
                         <div className="col-6">
                             <VideoPlayer dtype={"display"} src={this.state.correctFlashcard.length > 0 ? this.state.correctFlashcard[0].flashcardRecording : null}/>
@@ -226,7 +228,7 @@ class ViewFlashCard extends React.Component {
 
                         <div className="col-6">
                             {this.state.showRecorder && <VideoRecorder handleRecording={this.handleRecording}/>}
-                            {this.state.showSubmissionViewer &&  <VideoPlayer dtype={"submission"} create={this.state} src={ this.state.correctFlashcard[0].submission.filter(submission => submission.id === this.state.submissionId)[0].flashcardSubmissionRecording}/>}
+                            {this.state.showSubmissionViewer &&  <VideoPlayer dtype={"submission"} create={this.state} src={this.state.correctFeedback[0].flashcardSubmissionRecording}/>}
                             {this.state.showRecorder && 
                             <div className={classes.buttoncontainer}> 
                              <button onClick={(e)=>{this.addSubmission(e)}}>Add Submission</button>
