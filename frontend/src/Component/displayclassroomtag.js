0825e6281b8b0bbc4c7fbde9e65e9209421587b1
Brian Cheng <brianhoi322@gmail.com>
2021-09-22 09:23:22 +0800

    Beautified create quizcard

--- frontend/src/Component/displayclassroomtag.js
+++ frontend/src/Component/displayclassroomtag.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux'
 
+import { deleteTag } from '../Redux/actions/tagAction';
 import classes from './displayclassroomtag.module.css'
 
 class PureDisplayClassroomTag extends React.Component {
 
+    deleteTag(tagId){
+        this.props.deleteTag({
+            tag: tagId
+        })
+    }
+    
     render() {
         console.log("CLASSROOM TAG PROPS",this.props.tags);
         return (
             <>
                 {this.props.tags && this.props.tags.length > 0 ?this.props.tags.map((tag, i) => {
                         return (
-                            <span key={i} data-key={i} className={classes.tagbutton}>
+                            <span key={i} data-key={i} onClick={()=>this.deleteTag(tag.id)} data-key="delete" className={classes.tagbutton}>
                                 #{tag.body}
                             </span>
                         )
@@ -22,5 +29,13 @@ class PureDisplayClassroomTag extends React.Component {
     }
 }
 
+const mapDispatchToProps = dispatch => {
+    return {
+        deleteTag: (tag) => {
+            console.log("Delete Tag", tag)
+            dispatch(deleteTag(tag))
+        }
+    }
+}
 
-export const DisplayClassroomTag = connect(null, null)(PureDisplayClassroomTag)
+export const DisplayClassroomTag = connect(null, mapDispatchToProps)(PureDisplayClassroomTag)
