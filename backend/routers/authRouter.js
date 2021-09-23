3fbd897eb957e52ad772e7b61ec5dc4dbda2403d
ih810 <ivanhong810@gmail.com>
2021-09-23 18:49:58 +0800

    mergin main

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -21,12 +21,13 @@ class AuthRouter {
     }
 
     async postLocal(req, res) {
-        console.log("Requesting local login")
+        console.log("Requesting local login", req.body)
         if (req.body.email && req.body.password) {
             let user = await this.knex("user").where({
                 email: req.body.email
             }) 
             if (user) {
+                console.log(user)
                 let result = await hashFunction.checkPassword(req.body.password, user[0].passwordHash)
                 if (result) {
                     let payload = {
