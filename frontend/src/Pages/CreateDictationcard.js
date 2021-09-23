ee1963fcfd6b8738ee7162f83f9dce7b0dd00d43
ih810 <ivanhong810@gmail.com>
2021-09-23 21:48:24 +0800

    fixed two input box in create dictationcard

--- frontend/src/Pages/CreateDictationcard.js
+++ frontend/src/Pages/CreateDictationcard.js
@@ -61,7 +61,7 @@ class CreateDictationcard extends React.Component {
         e.preventDefault();
         if (this._inputElement.value !== "") {
             var newItem = {
-                text: this._inputElement.value,
+                dictationcardBody: this._inputElement.value,
                 key: Date.now(),
                 dictationRecording: ""
             };
@@ -145,7 +145,6 @@ class CreateDictationcard extends React.Component {
 
                                 </form>
                             </div>
-
                             <div className="col col-12">
                                 <DisplayEntries handleRecording={(key, fileName) => this.handleRecording(key, fileName)} entries={this.state.items} delete={this.deleteItem} />
                             </div>
